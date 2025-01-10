9c8bdac643a60819222891685c9f43424ffc290d
Midget807 <phu7mai@gmail.com>
2025-01-10 22:46:03 +1030

    Initial commit

---
+++ src/main/java/com/bcsmp/BCSMPS2ContentDataGenerator.java
@@ -0,0 +1,11 @@
+package com.bcsmp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BCSMPS2ContentDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/bcsmp/client/BCSMPS2ContentClient.java
@@ -0,0 +1,10 @@
+package com.bcsmp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BCSMPS2ContentClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
