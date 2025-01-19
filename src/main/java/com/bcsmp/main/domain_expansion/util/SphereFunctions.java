32f702d1c06e87a77f7cd86cb9738a1af8416800
Midget807 <121651013+Midget807@users.noreply.github.com>
2025-01-19 16:52:59 +0900

    Update SphereFunctions.java

--- src/main/java/com/bcsmp/main/domain_expansion/util/SphereFunctions.java
+++ src/main/java/com/bcsmp/main/domain_expansion/util/SphereFunctions.java
@@ -2,4 +2,7 @@ package com.bcsmp.main.domain_expansion.util;
 
 public class SphereFunctions {
     
+    public void domainSphereIndicator(ParticleTypes particle, float domainRadius) {
+        
+    }
 }
