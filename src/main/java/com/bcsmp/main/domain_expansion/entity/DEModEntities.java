def98e30e794318cc5026b30dd9a62da892a1c41
Midget807 <121651013+Midget807@users.noreply.github.com>
2025-01-19 20:03:53 +0900

    Update DEModEntities.java

--- src/main/java/com/bcsmp/main/domain_expansion/entity/DEModEntities.java
+++ src/main/java/com/bcsmp/main/domain_expansion/entity/DEModEntities.java
@@ -3,7 +3,7 @@ package com.bcsmp.main.domain_expansion.entity;
 public class DEModEntities {
     
 
-    public static void registerModEntities() {
+    public static void registerDomainExpansionEntities() {
         BCSMPS2ContentMain.LOGGER.info("Domain Expansion -> Registering Mod Entities");
     }
 }
