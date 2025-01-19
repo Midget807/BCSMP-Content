1de0fde3d0b37676229233888281650bb2ce9581
Midget807 <121651013+Midget807@users.noreply.github.com>
2025-01-19 18:55:17 +0900

    Update DEModComponents.java

--- src/main/java/com/bcsmp/main/domain_expansion/cca/DEModComponents.java
+++ src/main/java/com/bcsmp/main/domain_expansion/cca/DEModComponents.java
@@ -1,10 +1,11 @@
 package com.bcsmp.main.domain_expansion.cca;
 
 public class DEModComponents implements WorldComponentInitializer {
-    public static final ComponentKey<BooleanCompoment> DEF_DOMAIN_1_IN_USE = ComponentRegistry.getOrCreate(BCSMPS2Main.);
+    public static final ComponentKey<BooleanCompoment> DEF_DOMAIN_1_IN_USE = 
+        ComponentRegistry.getOrCreate(BCSMPS2ContentMain.domainExpansionId("def_domain_1_in_use"), BooleanComponent.class);
 
     @Override
     public void registerWorldComponentFactories(WorldComponentFactoryRegistry registry) {
-        
+        registry.register(DEF_DOMAIN_1_IN_USE, WorldBooleanComponent::new); //todo make "WorldBooleanComponent" custom component class
     }
 }
