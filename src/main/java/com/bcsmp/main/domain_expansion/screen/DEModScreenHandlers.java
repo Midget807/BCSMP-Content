3cd5dd4fae9bb132d35981667163104f79da5612
Midget807 <121651013+Midget807@users.noreply.github.com>
2025-01-20 17:12:57 +0900

    Update DEModScreenHandlers.java

--- src/main/java/com/bcsmp/main/domain_expansion/screen/DEModScreenHandlers.java
+++ src/main/java/com/bcsmp/main/domain_expansion/screen/DEModScreenHandlers.java
@@ -1,3 +1,8 @@
 package com.bcsmp.main.domain_expansion.screen;
 
-public class DEModScreenHandlers {}
+public class DEModScreenHandlers {
+
+    public static void registerDomainExpansionScreenHandlers() {
+        BCSMPS2ContentMain.LOGGER.info("Domain Expansion -> Registering Mod Screen Handlers");
+    }
+}
