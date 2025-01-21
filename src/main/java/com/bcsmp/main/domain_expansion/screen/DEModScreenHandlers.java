558ff70720ad3cc4b73b85cb08cf1bac759d9e03
Midget807 <121651013+Midget807@users.noreply.github.com>
2025-01-21 10:58:34 +0900

    Update DEModScreenHandlers.java

--- src/main/java/com/bcsmp/main/domain_expansion/screen/DEModScreenHandlers.java
+++ src/main/java/com/bcsmp/main/domain_expansion/screen/DEModScreenHandlers.java
@@ -1,6 +1,9 @@
 package com.bcsmp.main.domain_expansion.screen;
 
 public class DEModScreenHandlers {
+    public static final ScreenHandlerType<DomainWindowScreenHandler> DOMAIN_WINDOW_SCREEN_HANDLER =
+        Registry.register(Registries.SCREEN_HANDLER, BCSMPS2ContentMain.domainExpansionId("domain_window_screen_handler"),
+            new ScreenHandlerType<>(DomainWindowScreenHandler::new, FeatureFlags.VANILLA_FEATURES));
 
     public static void registerDomainExpansionScreenHandlers() {
         BCSMPS2ContentMain.LOGGER.info("Domain Expansion -> Registering Mod Screen Handlers");
