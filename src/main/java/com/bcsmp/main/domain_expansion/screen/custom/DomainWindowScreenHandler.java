5bcf7eac32b86f848edce8064c212ecfd3e6e4b1
Midget807 <121651013+Midget807@users.noreply.github.com>
2025-01-21 14:05:18 +0900

    Update DomainWindowScreenHandler.java

--- src/main/java/com/bcsmp/main/domain_expansion/screen/custom/DomainWindowScreenHandler.java
+++ src/main/java/com/bcsmp/main/domain_expansion/screen/custom/DomainWindowScreenHandler.java
@@ -7,5 +7,10 @@ public class DomainWindowScreenHandler extends ScreenHandler {
     }
     public DomainWindowScreenHandler(ScreenHandlerType<?> type, int syncId) {
         super(type, syncId);
+        //adds available domain occupance slots
+        for (int i = 0; i < 10; i++) {
+            this.addSlot(new Slot(<inventory>, i, 17 + ));
+        }
+        
     }
 }
